facd9d9720a4cc0a512efac864521109b01523b1
Ubuntu <ubuntu@ip-172-31-31-167.ap-northeast-2.compute.internal>
2019-03-05 15:12:49 +0000

    Change setup.sh

--- setup.sh
+++ setup.sh
@@ -1,40 +1,47 @@
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as sudo" 
+   exit 1
+fi
+
+
+INSTANCE_NUM=${1:-7}
+
 # installing eksctl
-# installing eksctl
-echo '>> Installing eksctl...'
+echo '>>>>>>>> Installing eksctl >>>>>>>>>'
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 mv /tmp/eksctl /usr/local/bin
-echo '>> done'
+echo '>>>>>>>> done >>>>>>>>>'
 
 
 # installing heptio-authenticator
-echo '>> Installing aws-iam-authenticator...'
+echo '>>>>>>>> Installing aws-iam-authenticator >>>>>>>>>'
 curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.11.5/2018-12-06/bin/linux/amd64/aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
 mv aws-iam-authenticator /usr/local/bin
-echo '>> done'
+echo '>>>>>>>> done >>>>>>>>>'
 
 
 # installing kubectl
-echo '>> Installing kubectl...'
+echo '>>>>>>>> Installing kubectl >>>>>>>>>'
 apt-get update && apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubectl
-echo '>> done'
+echo '>>>>>>>> done >>>>>>>>>'
 
 
 # installing helm client
-echo '>> Installing helm client...'
+echo '>>>>>>>> Installing helm client >>>>>>>>>'
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get | bash
-echo '>> done'
+echo '>>>>>>>> done >>>>>>>>>'
 
 
 # k8s cluster
-echo '>> Creating k8s cluster...'
-echo 'eksctl create cluster --name eks-ml --nodes 20 --node-type m5.xlarge'
-echo '>> This will take a while'
-eksctl create cluster --name eks-ml --nodes 20 --node-type m5.xlarge
+echo '>>>>>>>> Creating k8s cluster >>>>>>>>>'
+echo "eksctl create cluster --name eks-ml --nodes $INSTANCE_NUM --node-type m5.xlarge"
+echo '>>>>>>>> This will take a while'
+eksctl create cluster --name eks-ml --nodes $INSTANCE_NUM --node-type m5.xlarge
 sleep 20
 
 for node in $(kubectl get node | cut -d ' ' -f1 | sed 1d)
@@ -43,11 +50,11 @@ do
     kubectl label node $node ml-type=$ml_type
 done
 
-echo '>> done'
+echo '>>>>>>>> done >>>>>>>>>'
 
 
 # installing metric server
-echo '>> Installing metirc server...'
+echo '>>>>>>>> Installing metric server >>>>>>>>>'
 cat <<EOF | kubectl create -f -
 apiVersion: v1
 kind: ServiceAccount
@@ -72,4 +79,5 @@ EOF
 helm init --service-account tiller
 sleep 20
 helm install stable/metrics-server --name stats --namespace kube-system --set args={--logtostderr,--metric-resolution=2s}
-echo '>> done'
+chown $SUDO_UID:$SUDO_GID $HOME/.kube/config
+echo '>>>>>>>> done >>>>>>>>>'
