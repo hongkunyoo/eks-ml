817217b277dfc4d0c9511d18d8e2522da572fa2d
hongkunyoo <hongkunyoo@gmail.com>
2019-03-11 13:36:21 +0000

    Change bugs

--- setup.sh
+++ setup.sh
@@ -39,9 +39,9 @@ echo '>>>>>>>> done >>>>>>>>>'
 
 # k8s cluster
 echo '>>>>>>>> Creating k8s cluster >>>>>>>>>'
-echo "eksctl create cluster --name $CLUSTER_NAME --nodes $INSTANCE_NUM --node-type m5.xlarge"
+echo "eksctl create cluster --name $CLUSTER_NAME --node-type m5.xlarge --nodes-min=5 --nodes-max=10"
 echo '>>>>>>>> This will take a while'
-eksctl create cluster --name eks-ml --nodes $INSTANCE_NUM --node-type m5.xlarge
+eksctl create cluster --name $CLUSTER_NAME --node-type m5.xlarge --nodes-min=5 --nodes-max=10
 sleep 20
 
 for node in $(kubectl get node | cut -d ' ' -f1 | sed 1d)
@@ -80,20 +80,5 @@ helm init --service-account tiller
 sleep 20
 helm install stable/metrics-server --name stats --namespace kube-system --set 'args={--logtostderr,--metric-resolution=2s}'
 chown $SUDO_UID:$SUDO_GID $HOME/.kube/config
-echo '>>>>>>>> done >>>>>>>>>'
-
-
-
-echo '>>>>>>>> Installing Cluster auto scaler >>>>>>>>>'
-NG_ID=$(eksctl get nodegroup --cluster $CLUSTER_NAME | cut -d ' ' -f1 | sed 1d | cut -f2)
-NG_STACK=eksctl-$CLUSTER_NAME-nodegroup-$NG_ID
-ASG_ID=$(aws cloudformation describe-stack-resource --stack-name $NG_STACK --logical-resource-id NodeGroup --query StackResourceDetail.PhysicalResourceId --output text)
-
-aws autoscaling create-or-update-tags --tags ResourceId=$ASG_ID,ResourceType=auto-scaling-group,Key=k8s.io/cluster-autoscaler/enabled,Value=,PropagateAtLaunch=true
-aws autoscaling create-or-update-tags --tags ResourceId=$ASG_ID,ResourceType=auto-scaling-group,Key=k8s.io/cluster-autoscaler/$CLUSTER_NAME,Value=,PropagateAtLaunch=true
-
-NODE_ROLE=$(aws cloudformation describe-stack-resource --stack-name $NG_STACK --logical-resource-id NodeInstanceRole --query StackResourceDetail.PhysicalResourceId --output text)
-aws iam put-role-policy --role-name $NODE_ROLE --policy-name autoscale --policy-document '{ "Version": "2012-10-17", "Statement": [ { "Effect": "Allow",  "Action": [ "autoscaling:*" ], "Resource": "*" } ] }'
-
-helm install stable/cluster-autoscaler --name autoscale --namespace kube-system --set autoDiscovery.clusterName=$CLUSTER_NAME,awsRegion=ap-northeast-2,sslCertPath=/etc/kubernetes/pki/ca.crt
+chown -R $SUDO_UID:$SUDO_GID $HOME/.helm
 echo '>>>>>>>> done >>>>>>>>>'
